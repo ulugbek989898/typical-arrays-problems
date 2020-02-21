ac1a3369add25b159f039d1145f82a8a484aa9c1
ulugbek989898 <u.isroilov@student.inha.uz>
2020-02-21 14:06:30 +0500

    my commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array=[]) {
+    let len = array.length;
+    if (len > 0) {
+        let res = array[0];
+        for (let i = 1; i < len; i++){
+            if( res > array[i]){
+                res = array[i];
+            }
+        }
+        return res;
+    }
+    else{
+        return 0;
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array=[]) {
+    let len = array.length;
+    if (len > 0) {
+        let res = array[0];
+        for (let i = 1; i < len; i++){
+            if( res < array[i]){
+                res = array[i];
+            }
+        }
+        return res;
+    }
+    else{
+        return 0;
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array=[]) {
+    let len = array.length;
+    if (len > 0) {
+        let res = array[0];
+        for (let i = 1; i < len; i++){
+            res += array[i];
+        }
+        return res/len;
+    }
+    else{
+        return 0;
+    }
 }
